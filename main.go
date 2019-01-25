2c8e49347f943e29f011fef9e501f35d147317cf
Alexsoul133 <alexsoul133@gmail.com>
2019-01-25 17:27:53 +0300

    add database

--- main.go
+++ main.go
@@ -1,12 +1,99 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
+var host = os.Getenv("HOST")
+var port = os.Getenv("PORT")
+var user = os.Getenv("USER")
+var password = os.Getenv("PASSWORD")
+var dbname = os.Getenv("DBNAME")
+var sslmode = os.Getenv("SSLMODE")
+
+var dbInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+	host,
+	port,
+	user,
+	password,
+	dbname,
+	sslmode)
+
+type SearchResults struct {
+	ready   bool
+	Query   string
+	Results []Result
+}
+
+type Result struct {
+	Name, Description, URL string
+}
+
+func (sr *SearchResults) UnmarshalJSON(bs []byte) error {
+	array := []interface{}{}
+	if err := json.Unmarshal(bs, &array); err != nil {
+		return err
+	}
+	sr.Query = array[0].(string)
+	for i := range array[1].([]interface{}) {
+		sr.Results = append(sr.Results, Result{
+			array[1].([]interface{})[i].(string),
+			array[2].([]interface{})[i].(string),
+			array[3].([]interface{})[i].(string),
+		})
+	}
+	return nil
+}
+
+//Создаем таблицу users в БД при подключении к ней
+func createTable() error {
+
+	//Подключаемся к БД
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	//Создаем таблицу users
+	if _, err = db.Exec(`CREATE TABLE users(ID SERIAL PRIMARY KEY, TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP, USERNAME TEXT, CHAT_ID INT, MESSAGE TEXT, ANSWER TEXT);`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//Собираем данные полученные ботом
+func collectData(username string, chatid int64, message string, answer []string) error {
+
+	//Подключаемся к БД
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	//Конвертируем срез с ответом в строку
+	answ := strings.Join(answer, ", ")
+
+	//Создаем SQL запрос
+	data := `INSERT INTO users(username, chat_id, message, answer) VALUES($1, $2, $3, $4);`
+
+	//Выполняем наш SQL запрос
+	if _, err = db.Exec(data, `@`+username, chatid, message, answ); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI("430629496:AAHDBvxHimRzeURldxAz_4v8pp4bKzoeH8s")
